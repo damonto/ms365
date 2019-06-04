5910fed755285183ff883f2578ee1f1aab335561
Damon To <damon.tu@outlook.com>
2019-06-04 16:08:13 +0800

    Add list subscribed skus

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module office365
 go 1.12
 
 require (
-	github.com/bluele/gcache v0.0.0-20190518031135-bc40bd653833
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sessions v0.0.0-20190512062852-3cb4c4f2d615
 	github.com/gin-gonic/gin v1.4.0
 	github.com/smartystreets/goconvey v0.0.0-20190330032615-68dc04aab96a // indirect
--- request/request.go
+++ request/request.go
@@ -35,18 +35,52 @@ func GetAccessToken(code string) (string, error) {
 }
 
 // GetSubscribedSkus 订阅的 Skus
-func GetSubscribedSkus(accessToken string) {
+func GetSubscribedSkus(accessToken string) ([]interface{}, error) {
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", "https://graph.microsoft.com/v1.0/subscribedSkus", nil)
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
 
 	resp, err := client.Do(req)
-	if err == nil {
-		var respJSON map[string]interface{}
-		defer resp.Body.Close()
-		json.NewDecoder(resp.Body).Decode(&respJSON)
-
-		fmt.Println(respJSON)
+	if err != nil {
+		return make([]interface{}, 0), err
 	}
 
+	var respJSON map[string]interface{}
+	defer resp.Body.Close()
+	json.NewDecoder(resp.Body).Decode(&respJSON)
+
+	if respJSON["error"] != nil {
+		return make([]interface{}, 0), fmt.Errorf("%v", respJSON["error"].(map[string]interface{})["code"])
+	}
+
+	return filterSubscribedSkus(respJSON), nil
 }
+
+// Sku 可用的 SKU
+type Sku struct {
+	SkuID        interface{}
+	Total        interface{}
+	Used         interface{}
+	FriendlyName string
+}
+
+func filterSubscribedSkus(respJSON map[string]interface{}) []interface{} {
+	subscribedSkus := respJSON["value"].([]interface{})
+
+	var availableSkus []interface{}
+	for _, sku := range subscribedSkus {
+		sku := sku.(map[string]interface{})
+		skuPartNum := fmt.Sprintf("%v", sku["skuPartNumber"])
+		if sku["capabilityStatus"] == "Enabled" && Skus[skuPartNum] != "" {
+			availableSkus = append(availableSkus, &Sku{
+				SkuID:        sku["skuId"],
+				Total:        sku["prepaidUnits"].(map[string]interface{})["enabled"],
+				Used:         sku["consumedUnits"],
+				FriendlyName: Skus[skuPartNum],
+			})
+		}
+	}
+
+	return availableSkus
+}
+
