82d8a19419812abe0e3bc41991b30b41bde9121a
Damon To <damon.tu@outlook.com>
2019-06-10 16:37:35 +0800

    Update debug mode

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"office365/config"
 	"office365/model"
 	"office365/routes"
-
-	"github.com/gin-gonic/gin"
 )
 
 func init() {
@@ -15,10 +13,6 @@ func init() {
 
 func main() {
 	r := routes.InitRoutes()
-
-	if !config.RuntimeConfig.Debug {
-		gin.SetMode(gin.ReleaseMode)
-	}
 
 	r.Run()
 }
