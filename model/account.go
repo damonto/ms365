6fec7093880a9efc7a363f72d9d9fb18a10d4cc0
Damon To <damon.tu@outlook.com>
2019-08-27 16:50:59 +0800

    Fix: failed to AutoMigrate

--- model/account.go
+++ model/account.go
@@ -6,12 +6,12 @@ import (
 
 // Account 用户表
 type Account struct {
-	ID           uint       `gorm:"primary_key" json:"-"`
+	ID           uint       `gorm:"primary_key;column:id" json:"-"`
 	UserID       string     `gorm:"type:varchar(255);not null;column:user_id" json:"user_id"`
 	Email        string     `gorm:"type:varchar(255);not null;column:email" json:"email"`
 	AccessToken  string     `gorm:"type:text;not null;column:access_token" json:"-"`
 	RefreshToken string     `gorm:"type:text;not null;column:refresh_token" json:"-"`
-	ExpiresIn    int        `gorm:"type:int:10;not null;column:expires_in" json:"-"`
+	ExpiresIn    int        `gorm:"type:int(10);not null;column:expires_in" json:"-"`
 	CreatedAt    time.Time  `json:"created_at"`
 	UpdatedAt    time.Time  `json:"updated_at"`
 	DeletedAt    *time.Time `json:"-"`
