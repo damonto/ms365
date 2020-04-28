e867195710a473005c3ba15e8f737c9f603b7b8b
Damon To <damon.tu@outlook.com>
2020-04-28 19:08:04 +0800

    fix: cors allow origin

--- internal/app/middleware/cors.go
+++ internal/app/middleware/cors.go
@@ -7,8 +7,8 @@ import (
 // CORS is a middleware to allow cors requests
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", c.GetHeader("origin"))
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "false")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Accept, Origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
 
